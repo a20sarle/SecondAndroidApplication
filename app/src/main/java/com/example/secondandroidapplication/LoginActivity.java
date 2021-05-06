1bd96c2628790864013560e69041164228403224
a20sarle <a20sarle.student@his.se>
2021-05-06 14:41:49 +0200

    added a second screen (login)

---
+++ app/src/main/java/com/example/secondandroidapplication/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.secondandroidapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG","Hej!");
+                Intent intent = new Intent(com.example.secondandroidapplication.LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
